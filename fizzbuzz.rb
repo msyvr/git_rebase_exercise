c4818ccbe90ea0caae01dc2995edcd1fe7cfd075
Tammam Kbeili <tammam.kbeili@gmail.com>
2015-11-09 08:11:13 -0800

    make the first condition divide by 15 instead of 3 and 5

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,8 +5,8 @@ print "Give me a word (e.g. BUZZ): "
 second_word = gets.chomp
 
 for x in 1..100
-  if x % 3 == 0 && x % 5 == 0
-    puts "#{first_word}#{second_word}"
+  if x % 15 == 0
+    puts "FIZZBUZZ"
   elsif x % 5 == 0
     puts second_word
   elsif x % 3 == 0
